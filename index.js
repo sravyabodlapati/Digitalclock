f70dd4229c404187d3ea540133c7e0e1caeb1d13
sravyabodlapati <117891470+sravyabodlapati@users.noreply.github.com>
2022-11-11 18:21:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function currentTime() {
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session = "AM";
+
+
+if (h === 0) {
+    h = 12 ;
+}
+
+if (h > 12) {
+    h = h - 12;
+    session = "PM";
+}
+
+ h = h < 10 ? "0" + h : h
+ m = m < 10 ? "0" + m : m
+ s = s < 10 ? "0" + s : s
+ var time = h + ":" + m +":"+ s +"" + session;
+
+ //var time = '${h} : ${m} : ${s} ${session}'
+ document.getElementById("digitalclock").innerText = time;
+ document.getElementById("digitalclock").textContent = time;
+
+
+ setTimeout(currentTime,1000);
+}
+currentTime();
+
+
+
